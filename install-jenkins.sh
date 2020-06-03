a57ba486f8c82357693d3345607fddd44375ffda
Kim Oun Ju (김운주) <kimounju@gmail.com>
2020-06-03 15:26:26 +0900

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,3 +1,10 @@
 helm init
 helm install --name andy-nginx-ingress stable/nginx-ingress
 helm install --name andy-jenkins stable/jenkins -f jenkins-values.yaml
+helm install --name andy-prometheus stable/prometheus
+helm install --name andy-metric-server stable/metrics-server
+# Add the Elastic Helm Chart Repo
+helm repo add elastic https://helm.elastic.co
+helm install --name andy-elasticsearch elastic/elasticsearch -f elasticsearch-values.yaml
+helm install --name andy-kibana elastic/kibana -f kibana-values.yaml
+helm install --name andy-openldap stable/openldap
