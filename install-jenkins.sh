8c05bbaa4340d1464f69230f3a4a393d96cb9122
Kim Oun Ju (김운주) <kimounju@gmail.com>
2020-06-04 16:23:49 +0900

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,15 +1,9 @@
 helm init
 ## Nginx 설치
 helm install --name andy-nginx-ingress stable/nginx-ingress
-## Jenkins 설치
+## Jenkins 설치, 암호: 1111
 helm install --name andy-jenkins stable/jenkins -f jenkins-values.yaml
-#helm install --name andy-prometheus stable/prometheus
-#helm install --name andy-metric-server stable/metrics-server
-## Add the Elastic Helm Chart Repo
-#helm repo add elastic https://helm.elastic.co
-#helm install --name andy-elasticsearch elastic/elasticsearch -f elasticsearch-values.yaml
-#helm install --name andy-kibana elastic/kibana -f kibana-values.yaml
-#helm install --name andy-openldap stable/openldap
+## http://127.0.0.1:80 으로 접속
 ## Nexus 설치
 helm repo add oteemocharts https://oteemo.github.io/charts
 helm install --name andy-sonatype-nexus oteemocharts/sonatype-nexus -f sonatype-nexus-values.yaml
@@ -17,3 +11,11 @@ helm install --name andy-sonatype-nexus oteemocharts/sonatype-nexus -f sonatype-
 ## MySQL 설치
 helm install --name andy-mysql stable/mysql -f mysql-values.yaml
 ## root 암호는 1111, localhost:32000으로 접속, DataBase INF 생성됨
+
+#helm install --name andy-prometheus stable/prometheus
+#helm install --name andy-metric-server stable/metrics-server
+## Add the Elastic Helm Chart Repo
+#helm repo add elastic https://helm.elastic.co
+#helm install --name andy-elasticsearch elastic/elasticsearch -f elasticsearch-values.yaml
+#helm install --name andy-kibana elastic/kibana -f kibana-values.yaml
+#helm install --name andy-openldap stable/openldap
