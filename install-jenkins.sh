ac6656807bf5f6179f62dfebdcc0930421f4eaa4
Kim Oun Ju (김운주) <kimounju@gmail.com>
2020-06-04 10:54:43 +0900

    Update install-jenkins.sh

--- install-jenkins.sh
+++ install-jenkins.sh
@@ -1,15 +1,15 @@
 helm init
 helm install --name andy-nginx-ingress stable/nginx-ingress
 helm install --name andy-jenkins stable/jenkins -f jenkins-values.yaml
-helm install --name andy-prometheus stable/prometheus
-helm install --name andy-metric-server stable/metrics-server
-# Add the Elastic Helm Chart Repo
-helm repo add elastic https://helm.elastic.co
-helm install --name andy-elasticsearch elastic/elasticsearch -f elasticsearch-values.yaml
-helm install --name andy-kibana elastic/kibana -f kibana-values.yaml
-helm install --name andy-openldap stable/openldap
+#helm install --name andy-prometheus stable/prometheus
+#helm install --name andy-metric-server stable/metrics-server
+## Add the Elastic Helm Chart Repo
+#helm repo add elastic https://helm.elastic.co
+#helm install --name andy-elasticsearch elastic/elasticsearch -f elasticsearch-values.yaml
+#helm install --name andy-kibana elastic/kibana -f kibana-values.yaml
+#helm install --name andy-openldap stable/openldap
 helm repo add oteemocharts https://oteemo.github.io/charts
 helm install --name andy-sonatype-nexus oteemocharts/sonatype-nexus -f sonatype-nexus-values.yaml
-# Nexus 포트 포워딩 설정
+## Nexus 포트 포워딩 설정
 kubectl --namespace default port-forward $POD_NAME 8081
 
