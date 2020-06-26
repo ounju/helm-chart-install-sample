3c9a599b302023ada5d0f97a96447c3043cc8f21
ounju.kim <ounju.kim@bespinglobal.com>
2020-06-26 17:55:13 +0900

    1

--- spinnaker/config-spinnaker.sh
+++ spinnaker/config-spinnaker.sh
@@ -11,9 +11,7 @@ hal deploy apply
 ## Jenkins 활성화
 hal config ci jenkins enable
 ## Jenkins 계정정보 등록
-hal config ci jenkins master add andyjenkinsserver001 --address http://jenkins.andy.com --username admin --password 1111
 hal config ci jenkins master add andyjenkinsserver002 --address http://10.100.251.221:8080 --username admin --password 1111
-hal config ci jenkins master add andyjenkinsserver003 --address http://10.100.251.221:30910 --username admin --password 1111
 ## 설정 확인
 hal config
 ## 변경된 설정 적용
