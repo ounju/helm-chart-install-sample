dc5db5cfc20ab7fb3c0bb070857821aecf3da1b4
ounju.kim <ounju.kim@bespinglobal.com>
2020-07-03 17:34:06 +0900

    1

--- cicd-pipeline/install-jenkins.sh
+++ cicd-pipeline/install-jenkins.sh
@@ -25,3 +25,24 @@ helm install --name andy-mariadb bitnami/mariadb -f mariadb-values.yaml
 ## MySQL 설치
 #helm install --name andy-mysql stable/mysql -f mysql-values.yaml
 ## root 암호는 1111, localhost:32000으로 접속, DataBase INF 생성됨
+
+helm repo add gitlab https://charts.gitlab.io/
+helm install gitlab gitlab/gitlab --set global.hosts.domain=gitlab.inf.com --set certmanager-issuer.email=kimounju@gmail.com --set global.edition=ce --set gitlab-runner.runners.privileged=true --set hosts.https=false
+## Root 암호
+# kubectl get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
+# helm get values gitlab > gitlab.yaml
+# helm upgrade gitlab gitlab/gitlab -f gitlab.yaml
+
+## Nginx 설치
+# helm install nginx-ingress stable/nginx-ingress
+
+helm repo add oteemocharts https://oteemo.github.io/charts
+helm install nexus oteemocharts/sonatype-nexus -f nexus-values.yaml
+## admin / admin123
+
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm install mariadb bitnami/mariadb -f mariadb-values.yaml
+## root / admin123
+
+## 127.0.0.1   docker.inf.com    nexus.inf.com    mariadb.inf.com    gitlab.inf.com    registry.inf.com    minio.inf.com
+
